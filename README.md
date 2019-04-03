d098ead108178e4b18157307a05d453bb07712c0
Andrew Abbott <andrew.abbott@dynagility.com>
2019-04-03 08:55:28 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# ts-express
+
+A library of decorators to define class based "controller" classes.
+
+Controllers are specify a path and are used to generate an express Router.
+
+InitializeExpress creates (and returns) an express app with our most commonly used middleware (morgan for logging, body-parser, and express-prom-bundle).
+
+Default port is loaded from envvar SERVER_PORT and set to 3000 by default if no var is set. Controller classes passed in will be instantiated then transformed into routers. The router is then applied to the path specified in their decorator.
+
+```typescript
+function InitializeExpress(port: number = PORT, name = 'ExpressJS', middlware = [], ...controllers: any)
+```
+Example:
+
+```typescript
+@Controller('/api')
+class TestController {
+  constructor() {
+  }
+
+  @Get('/test')
+  Test(req: Request) {
+    return {'STATUS': 'OK'};
+  }
+}
+
+InitializeExpress(3000, 'Test App', [], TestController);
+```
---
+++ src/index.ts
@@ -0,0 +1,114 @@
+/*!
+  Copyright (c) 2019 DynAgility LLC. All rights reserved.
+  Licensed under the MIT License.
+*/
+
+import 'reflect-metadata';
+import express from 'express';
+import { Request, Response } from 'express';
+import * as bodyParser from 'body-parser';
+import { default as morgan } from 'morgan';
+import { default as promBundle } from 'express-prom-bundle';
+import cookieparser from 'cookie-parser';
+
+const API_BINDING_METADATA_KEY = Symbol('API_BINDING');
+const CONTROLER_PATH_KEY = Symbol('CONTROLLER_PATH');
+type httpmethods = 'get' | 'post' | 'put' | 'delete' | 'patch';
+
+export function Controller(path: string): Function {
+  return Reflect.metadata(CONTROLER_PATH_KEY, path);
+}
+
+function api(path: string, httpmethod: httpmethods, auth?: boolean) {
+  return Reflect.metadata(API_BINDING_METADATA_KEY, { path, httpmethod, auth});
+}
+
+export function Get(path: string, auth?: boolean) {
+  return api(path, 'get', auth);
+}
+
+export function Post(path: string, auth?: boolean) {
+  return api(path, 'post', auth);
+}
+
+export function Put(path: string, auth?: boolean) {
+  return api(path, 'put', auth);
+}
+
+export function Delete(path: string, auth?: boolean) {
+  return api(path, 'delete', auth);
+}
+
+export function Patch(path: string, auth?: boolean) {
+  return api(path, 'patch', auth);
+}
+
+const catchErrors = <T>(apiHandler: (req, res) => T) => async function(req: Request, res: Response) {
+  try {
+    const handlerValue: T = await apiHandler(req, res);
+    if (handlerValue !== undefined) {
+      res.send(handlerValue);
+    } else {
+      res.sendStatus(200);
+    }
+  } catch (e) {
+    console.log(`Received an error from handler ${apiHandler.name} : `, e);
+    res.statusCode = 500;
+    res.send({ error: 'Internal Server Error' });
+  }
+};
+
+function getRouterfromDecorators(controller: any, ...middlewares) {
+  const router = express.Router();
+  middlewares.forEach((middleware) => router.use(middleware));
+  const props = Object.getOwnPropertyNames(controller.constructor.prototype).map((method: string) => {
+    const decorator = Reflect.getMetadata(API_BINDING_METADATA_KEY, controller, method);
+    if (decorator === undefined) {
+      return false;
+    }
+    router[decorator.httpmethod](decorator.path, catchErrors(controller[method].bind(controller)));
+    return true;
+  });
+  if (!props.some((i) => i)) {
+    throw new TypeError(`GetRouterFromDecorators requires an obbject with Route Decorators. None were found on ${controller.name}.`);
+  }
+  return router;
+}
+
+const PORT = parseInt(process.env.SERVER_PORT || '3000');
+
+export function InitializeExpress(port: number = PORT, name = 'ExpressJS', additionalMiddleware: Array<any> = [], ...controllers: any) {
+  const app = express();
+  const metricsMiddleware = promBundle({ includeMethod: true });
+  const logger = morgan('tiny');
+  app.use(bodyParser.json());
+  app.use(cookieparser());
+  app.use(metricsMiddleware);
+  app.use(logger);
+
+  // Use any other middleware passed in
+  additionalMiddleware.forEach((m) => app.use(m));
+
+  // Create routers for each controller
+  controllers.forEach(controller => {
+    let instance = controller;
+    // We either get pre-initialized controllers or we need to initialize
+    if (typeof instance === 'function') {
+      instance = new controller();
+      // Make sure that whatever the controller function was gave us an instance
+      if (typeof instance !== 'object') {
+        console.log(`Received non-constructor controller ${instance.name}`);
+      }
+    }
+    const path = Reflect.getMetadata(CONTROLER_PATH_KEY, instance.constructor);
+    if (!path) {
+      // If we get a non-annotated class, skip it.
+      console.log(`Received controller ${instance.name} but did not have an @Controller annotation.`);
+      return;
+    }
+
+    app.use(path, getRouterfromDecorators(instance));
+  });
+  app.listen(port, () => console.log(`${name} listening on port ${port}`));
+  return app;
+}
